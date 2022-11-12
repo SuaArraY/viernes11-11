5147d7a40873f13e2e0c5a206ccdd69ab4caaab6
SuaArraY <1313178@senati.pe>
2022-11-11 19:05:17 -0500

    Diseño 2

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+   // public function index()
+    {
+        return view('alumno');
+    }
+
+    public function edit()
+    {
+        return view('alumno');
+    }
+
+    public function store()
+    {
+        return view('alumno');
+    }
+    public function guardar($_REQUEST $request)
+    {
+        return view('alumno');
+    }*//
+}
--- resources/views/alumno.blade.php
+++ resources/views/alumno.blade.php
@@ -1,25 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-{{-- <h3>Listado de Tareas</h3> --}}
-
-
-{{-- <div class=" container shadow p-3 mb-5 bg-white rounded">
-    <table  class="table table-stripped table-hover ">
-        <thead>
-            <tr>
-                <th>Nombre</th>
-                <th>Correo</th>
-                <th>Contraseña</th>
-                <th>Opciones</th>
-            </tr>
-        </thead>
-        <tbody>
-    
-           
-        </tbody>
-    </table> 
-</div> --}}
+{
 
 <div class="container ">
     <div class="row justify-content-center">
